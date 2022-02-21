647a3a834997f084dcd6bd579b6d2c3047548aa3
Narvak <91684269+Narvak@users.noreply.github.com>
2022-02-21 19:55:10 +0100

    Add files via upload

---
+++ javascript/lib-jquery.js
@@ -0,0 +1,10 @@
+function two() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#about").hide();
+            $("#gen_footer").show();
+            $("#div").show();
+            $("#two").show();
+        });
+    });
+}
