9cdc65946bd2377cbcc67b37a8bc705603ef42b5
Narvak <91684269+Narvak@users.noreply.github.com>
2022-02-22 10:58:34 +0100

    Add files via upload

--- javascript/lib-jquery.js
+++ javascript/lib-jquery.js
@@ -1,10 +1,54 @@
-function two() {
+function flux() {
     $(document).ready(function(){
         $("button").click(function(){
-            $("#about").hide();
+            $("#div").hide();
             $("#gen_footer").show();
-            $("#div").show();
-            $("#two").show();
+            $("#gen_middle").show();
+            $("#in_middle").show();
+        });
+    });
+}
+
+function search() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div").hide();
+            $("#gen_footer").show();
+            $("#gen_middle").show();
+            $("#in_middle").show();
+        });
+    });
+}
+
+function musique() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div").hide();
+            $("#gen_footer").show();
+            $("#gen_middle").show();
+            $("#in_middle").show();
+        });
+    });
+}
+
+function playlist() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div").hide();
+            $("#gen_footer").show();
+            $("#gen_middle").show();
+            $("#in_middle").show();
+        });
+    });
+}
+
+function Profil() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div").hide();
+            $("#gen_footer").show();
+            $("#gen_middle").show();
+            $("#in_middle").show();
         });
     });
 }
