e4b748f148bc69710996e48edc88a16a4a1872e8
Narvak <91684269+Narvak@users.noreply.github.com>
2022-03-13 16:36:01 +0100

    Add files via upload

---
+++ javascript/lib-jquery.js
@@ -0,0 +1,106 @@
+function flux() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div").hide();
+            $("#gen_footer").show();
+            $("#gen_middle").show();
+            $("#in_middle").show();
+        });
+    });
+}
+
+function search() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div").hide();
+            $("#gen_footer").show();
+            $("#gen_middle").show();
+            $("#in_middle").show();
+        });
+    });
+}
+
+function musique() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div").hide();
+            $("#gen_footer").show();
+            $("#gen_middle").show();
+            $("#in_middle").show();
+        });
+    });
+}
+
+function playlist() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div").hide();
+            $("#gen_footer").show();
+            $("#gen_middle").show();
+            $("#in_middle").show();
+        });
+    });
+}
+
+function Profil() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div").hide();
+            $("#gen_footer").show();
+            $("#gen_middle").show();
+            $("#in_middle").show();
+        });
+    });
+}
+
+
+
+
+function easteregg() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div").hide();
+            $("#gen_footer").hide();
+            $("#in_middle").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+
+            $("footer").show();
+        });
+    });
+}
+
+function showme() {
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("#div").hide();
+            $("#gen_footer").hide();
+            $("#in_middle").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+            $("#").hide();
+
+            $("footer").show();
+            $("#showme").show();
+        });
+    });
+}
